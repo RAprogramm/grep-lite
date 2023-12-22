9c9192641885ec75e6a494721c80d67cfcd8111e
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-12-22 18:58:00 +0900

    add: command line arguments parser

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,28 @@
-fn main() {
-    let search_term = "picture";
-    let qoute = "\
-Every face, every shop, bedroom window, puЬlic-house, and 
-dark square is а picture feverishly turned--in search of what? 
-It is the same with books. 
-What do we seek through millions of pages?";
+use clap::Parser;
+use regex::Regex;
 
-    for line in qoute.lines() {
-        if line.contains(search_term) {
-            println!("{}", line);
+/// Simple and light grep clone
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// The pattern to search for
+    pattern: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let re = Regex::new(&args.pattern).unwrap();
+
+    let quote = "Every face, every shop, bedroom window, puЬlic-house, and dark square 
+is а picture feverishly turned--in search of what? 
+It is the same with books. What do we seek through millions of pages?";
+
+    for line in quote.lines() {
+        let contains_substring = re.find(line);
+        match contains_substring {
+            Some(_) => println!("{}", line),
+            None => (),
         }
     }
 }
