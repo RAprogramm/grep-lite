9f7c111327da2d2c73ad93f185aaa8f2c88cdf40
RAprogramm <andrey.rozanov.vl@gmail.com>
2023-12-24 19:17:53 +0900

    add: reading from file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,19 @@
 use clap::Parser;
 use regex::Regex;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+
+fn process_lines<T: BufRead + Sized>(reader: T, re: Regex) {
+    for line_ in reader.lines() {
+        let line = line_.unwrap();
+        match re.find(&line) {
+            Some(_) => println!("{}", line),
+            None => (),
+        }
+    }
+}
 
 /// Simple and light grep clone
 #[derive(Parser, Debug)]
@@ -7,22 +21,22 @@ use regex::Regex;
 struct Args {
     /// The pattern to search for
     pattern: String,
+    /// File to search
+    input: String,
 }
 
 fn main() {
     let args = Args::parse();
-
     let re = Regex::new(&args.pattern).unwrap();
+    let input = &args.input;
 
-    let quote = "Every face, every shop, bedroom window, puЬlic-house, and dark square 
-is а picture feverishly turned--in search of what? 
-It is the same with books. What do we seek through millions of pages?";
-
-    for line in quote.lines() {
-        let contains_substring = re.find(line);
-        match contains_substring {
-            Some(_) => println!("{}", line),
-            None => (),
-        }
+    if input == "-" {
+        let stdin = io::stdin();
+        let reader = stdin.lock();
+        process_lines(reader, re);
+    } else {
+        let f = File::open(input).unwrap();
+        let reader = BufReader::new(f);
+        process_lines(reader, re);
     }
 }
